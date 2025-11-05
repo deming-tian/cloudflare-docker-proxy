7b105b1e3b6ca13586f688eb15c5a4bb739bee6d
deming-tian <136963391+deming-tian@users.noreply.github.com>
2025-11-05 15:02:27 +0800

    Update index.js
    自己的域名tdm.qzz.io

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + tdm.qzz.io]: dockerHub,
+  ["quay." + tdm.qzz.io]: "https://quay.io",
+  ["gcr." + tdm.qzz.io]: "https://gcr.io",
+  ["k8s-gcr." + tdm.qzz.io]: "https://k8s.gcr.io",
+  ["k8s." + tdm.qzz.io]: "https://registry.k8s.io",
+  ["ghcr." + tdm.qzz.io]: "https://ghcr.io",
+  ["cloudsmith." + tdm.qzz.io]: "https://docker.cloudsmith.io",
+  ["ecr." + tdm.qzz.io]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + tdm.qzz.io]: dockerHub,
 };
 
 function routeByHosts(host) {
